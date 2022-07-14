cb8bd259dc2dcf5058f3d694d765af3f62762521
Jash Parikh <jash.p@ahduni.edu.in>
2022-07-14 19:14:18 +0530

    Delete modules/car/car_inherit_practice directory

--- modules/car/car_inherit_practice/model/checkout_inherit.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api
-
-
-class CheckOutInherit(models.Model):
-    #_name = "checkout_inherit"
-    _inherit = "car.checkout"
-    _description = "Car Checkout"
-
-    booking_id = fields.Integer(string="booking id")
